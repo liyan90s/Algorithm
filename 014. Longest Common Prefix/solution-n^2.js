89ffc8dffab869774a5c8ba93620f760499eb193
Yan Li <y.yan.li@oracle.com>
2018-01-10 17:23:31 -0800

    update 014 sol

--- 014. Longest Common Prefix/solution-n^2.js
+++ 014. Longest Common Prefix/solution-n^2.js
@@ -7,17 +7,15 @@
 var longestCommonPrefix = function(strs) {
     if (strs === null || strs.length === 0) return "";
     
-    var firstStr = strs[0];
-    var longestLen = firstStr.length;
-    
+    var longestStr = strs[0];
     for (var i = 1; i < strs.length; i++) {
-        for (var j = 0; j < longestLen; j++) {
-            if (j >= strs[i].length || strs[i][j] !== firstStr[j]) {
+        for (var j = 0; j < longestStr.length; j++) {
+            if (j >= strs[i].length || strs[i][j] != longestStr[j]) {
+                longestStr = longestStr.substring(0, j);
                 break;
-            }
+            }            
         }
-        longestLen = j;
     }
     
-    return firstStr.substring(0, j);
+    return longestStr;
 };
