24fe364890b1b5d4b56d6e8b801bf4c461036485
Yan Li <y.yan.li@oracle.com>
2018-02-11 18:35:27 +0800

    add 027 sol

---
+++ Leet-027. Remove Element/solution-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        
+        return left;
+    }
+}
