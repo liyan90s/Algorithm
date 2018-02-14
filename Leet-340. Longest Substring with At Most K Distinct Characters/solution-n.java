af83e43e5dd66d496e55be255218e7ce4c36fd8f
Yan Li <y.yan.li@oracle.com>
2018-02-14 11:10:05 +0800

    change dir

--- Leet-340. Longest Substring with At Most K Distinct Characters/solution-n.java
+++ Leet-340. Longest Substring with At Most K Distinct Characters/solution-n.java
@@ -10,31 +10,32 @@ public class Solution {
             return 0;
         }
         
-        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;
         int right = 0;
         int count = 0;
-        int maxLen = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
         
         for (int left = 0; left < s.length(); left++) {
             while (right < s.length() && count <= k) {
-                if (map.containsKey(s.charAt(right)) && map.get(s.charAt(right)) > 0) {
+                if (map.containsKey(s.charAt(right))) {
                     map.put(s.charAt(right), map.get(s.charAt(right)) + 1);
                 } else {
-                    map.put(s.charAt(right), 1);
                     count++;
+                    map.put(s.charAt(right), 1);
                 }
                 right++;
             }
             
             if (count > k) {
                 maxLen = Math.max(maxLen, right - left - 1);
-            } else {                                                // need to check the case when right ptr hits the end
-                maxLen = Math.max(maxLen, s.length() - left);
+            } else if (right == s.length()) {               // need to check when right ptr hits the end !!
+                maxLen = Math.max(maxLen, right - left);
             }
             
-            if (map.containsKey(s.charAt(left)) && map.get(s.charAt(left)) > 0) {
+            if (map.containsKey(s.charAt(left))) {
                 map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                 if (map.get(s.charAt(left)) == 0) {
+                    map.remove(s.charAt(left));
                     count--;
                 }
             }
