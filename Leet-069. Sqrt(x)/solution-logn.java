e1d8bda443ef1f2574e6c13d842723e248f336c5
Yan Li <y.yan.li@oracle.com>
2018-02-17 10:28:54 +0800

    update sol for 069 and add README for lint390

--- Leet-069. Sqrt(x)/solution-logn.java
+++ Leet-069. Sqrt(x)/solution-logn.java
@@ -1,12 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
         if (x <= 0) {
-            return 0;
+            return x;
         }
         
         long left = 1, right = x;
         while (left <= right) {
             long mid = left + (right - left) / 2;
+            
             if (mid * mid > x) {
                 right = mid - 1;
             } else if (mid * mid < x) {
@@ -16,6 +17,6 @@ class Solution {
             }
         }
         
-        return (int) right;
+        return (int) right;     // we choose the lower bound if the result has decimal
     }
 }
