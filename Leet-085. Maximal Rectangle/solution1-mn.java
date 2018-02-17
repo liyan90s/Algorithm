b796ac0d9c5ed56dd0b07e01ef1064e6a2229e98
Yan Li <y.yan.li@oracle.com>
2018-02-17 08:38:38 +0800

    update sol for 085

--- Leet-085. Maximal Rectangle/solution1-mn.java
+++ Leet-085. Maximal Rectangle/solution1-mn.java
@@ -7,39 +7,37 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int[] heights = new int[n];
-        int maxArea = 0;
+        int max = 0;
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0) {
-                    heights[j] = matrix[0][j] - '0';
-                } else {
-                    heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
-                }
+                heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
             }
             
-            maxArea = Math.max(maxArea, findLargest(heights));
+            max = Math.max(max, findLargestRectangle(heights));
         }
         
-        return maxArea;
+        return max;
     }
     
-    public int findLargest(int[] heights) {
+    public int findLargestRectangle(int[] heights) {
         Stack<Integer> stack = new Stack<>();
-        
         int max = 0;
+        
         for (int i = 0; i < heights.length; i++) {
             while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
                 int h = heights[stack.pop()];
                 int w = stack.isEmpty() ? i : i - stack.peek() - 1;
                 max = Math.max(max, h * w);
             }
+            
             stack.push(i);
         }
         
         while (!stack.isEmpty()) {
             int h = heights[stack.pop()];
             int w = stack.isEmpty() ? heights.length : heights.length - stack.peek() - 1;
-            max = Math.max(max, h * w);            
+            max = Math.max(max, h * w);
         }
         
         return max;
