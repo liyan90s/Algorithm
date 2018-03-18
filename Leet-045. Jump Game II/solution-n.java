be7ae348dba59a2666f8e6aa4867fca64d45243b
Yan Li <y.yan.li@oracle.com>
2018-03-18 10:32:35 -0700

    update sol 045

--- Leet-045. Jump Game II/solution-n.java
+++ Leet-045. Jump Game II/solution-n.java
@@ -7,8 +7,13 @@ class Solution {
         int currEnd = 0, furthest = 0;
         int jump = 0;
         
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             furthest = Math.max(furthest, i + nums[i]);
+            
+            // check if it can jump to the end, if not, return -1
+            if (furthest <= i) {
+                return -1;
+            }
             
             if (i == currEnd) {
                 jump++;
