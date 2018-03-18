844653740cd99f28a30c5fc9aa48d01e8d376bb3
Yan Li <y.yan.li@oracle.com>
2018-03-18 09:41:38 -0700

    add sol 059

---
+++ Leet-059. Spiral Matrix II/solution-n^2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] generateMatrix(int n) {        
+        if (n <= 0) {
+            return new int[][] {};
+        }
+        
+        int[][] res = new int[n][n];
+        int left = 0, right = n - 1;
+        int top = 0, bottom = n - 1;
+        int num = 1;
+        
+        while (left <= right) {
+            for (int j = left; j <= right; j++) {
+                res[top][j] = num++;
+            }
+            top++;
+            
+            for (int i = top; i <= bottom; i++) {
+                res[i][right] = num++;
+            }
+            right--;
+            
+            for (int j = right; j >= left; j--) {
+                res[bottom][j] = num++;
+            }
+            bottom--;
+            
+            for (int i = bottom; i >= top; i--) {
+                res[i][left] = num++;
+            }
+            left++;
+        }
+        
+        return res;
+    }
+}
