2fa1970a83780d30cfbe162ee826063140f25d0f
Yan Li <y.yan.li@oracle.com>
2018-03-22 08:50:16 -0700

    add lint 447

---
+++ Lint-447. Search in a Big Sorted Array/solution-logn.java
@@ -0,0 +1,41 @@
+public class Solution {
+    /*
+     * @param reader: An instance of ArrayReader.
+     * @param target: An integer
+     * @return: An integer which is the first index of target.
+     */
+    public int searchBigSortedArray(ArrayReader reader, int target) {
+        if (reader == null) {
+            return -1;
+        }
+        
+        int left = 0;
+        int right = 1;
+
+        // find the upper bound
+        while (reader.get(right) < target) {
+            right <<= 1;
+        }
+        
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            int val = reader.get(mid);
+            
+            if (val == target || val > target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        
+        if (reader.get(left) == target) {
+            return left;
+        }
+        
+        if (reader.get(right) == target) {
+            return right;
+        }
+        
+        return -1;
+    }
+}
