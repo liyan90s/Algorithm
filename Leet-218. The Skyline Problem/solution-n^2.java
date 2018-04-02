cff893d3f1d171188c0381314cc17233feb2a2d7
Yan Li <y.yan.li@oracle.com>
2018-04-02 08:51:23 -0700

    update sol for leet 218

--- Leet-218. The Skyline Problem/solution-n^2.java
+++ Leet-218. The Skyline Problem/solution-n^2.java
@@ -1,6 +1,7 @@
 // Sweep Line
 // make the building start point to be a negative of its height, this helps when sorting, the
 // building starting point is always before the buildings end point.
+
 class Solution {
     public List<int[]> getSkyline(int[][] buildings) {
         List<int[]> res = new ArrayList<>();
@@ -8,38 +9,48 @@ class Solution {
             return res;
         }
         
-        List<int[]> points = new ArrayList<>();
+        List<Point> points = new ArrayList<>();
         for (int[] building : buildings) {
-            points.add(new int[] { building[0], -building[2] });
-            points.add(new int[] { building[1], building[2] });
+            points.add(new Point(building[0], -building[2]));
+            points.add(new Point(building[1], building[2]));
         }
         
-        Collections.sort(points, (p1, p2) -> {
-            if (p1[0] != p2[0]) {
-                return p1[0] - p2[0];
+        Collections.sort(points, (o1, o2) -> {
+            if (o1.x != o2.x) {
+                return o1.x - o2.x;
             } else {
-                return p1[1] - p2[1];
+                return o1.height - o2.height;
             }
         });
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>((i1, i2) -> i2 - i1);
+        // build maxHeap to make the heightest to be at the first place
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // add 0 into maxHeap for convenience calculating the ground-line (地平线)
         pq.offer(0);
         int prev = 0;
         
-        for (int[] point : points) {
-            if (point[1] < 0) {
-                pq.offer(-point[1]);
+        for (Point point : points) {
+            if (point.height < 0) {
+                pq.offer(-point.height);
             } else {
-                pq.remove(point[1]);        // cost O(n); TODO: use TreeMap to make it into O(logn)
+                pq.remove(point.height);   // cost O(n); TODO: use TreeMap to make it into O(logn)
             }
             
             int curr = pq.peek();
             if (curr != prev) {
-                res.add(new int[] {point[0], curr});
+                res.add(new int[] {point.x, curr});
                 prev = curr;
             }
         }
         
         return res;
+    }
+    
+    class Point {
+        int x, height;
+        public Point(int x, int height) {
+            this.x = x;
+            this.height = height;
+        }
     }
 }
--- Leet-218. The Skyline Problem/solution-nlogn.java
+++ Leet-218. The Skyline Problem/solution-nlogn.java
@@ -1,4 +1,7 @@
-// Time: O(nlogn)
+// Sweep Line
+// make the building start point to be a negative of its height, this helps when sorting, the
+// building starting point is always before the buildings end point.
+
 class Solution {
     public List<int[]> getSkyline(int[][] buildings) {
         List<int[]> res = new ArrayList<>();
@@ -6,38 +9,57 @@ class Solution {
             return res;
         }
         
-        List<int[]> points = new ArrayList<>();
+        List<Point> points = new ArrayList<>();
         for (int[] building : buildings) {
-            points.add(new int[] {building[0], -building[2]});
-            points.add(new int[] {building[1], building[2]});
+            points.add(new Point(building[0], -building[2]));
+            points.add(new Point(building[1], building[2]));
         }
-        Collections.sort(points, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p1[0] - p2[0]);
+        
+        Collections.sort(points, (o1, o2) -> {
+            if (o1.x != o2.x) {
+                return o1.x - o2.x;
+            } else {
+                return o1.height - o2.height;
+            }
+        });
         
         // use TreeMap instead of a PriorityQueue to make remove entry to be O(logn) time
-        TreeMap<Integer, Integer> heightMap = new TreeMap<>(Collections.reverseOrder()); 
+        TreeMap<Integer, Integer> heightMap = new TreeMap<>(Collections.reverseOrder());
         heightMap.put(0, 1);
         int prev = 0;
-        for (int[] point : points) {
-            if (point[1] < 0) {
-                Integer cnt = heightMap.get(-point[1]);
+        
+        for (Point point : points) {
+            if (point.height < 0) {
+                // found a building starting point, add it into heightMap
+                Integer cnt = heightMap.get(-point.height);
                 cnt = cnt == null ? 1 : cnt + 1;
-                heightMap.put(-point[1], cnt);
+                heightMap.put(-point.height, cnt);
             } else {
-                Integer cnt = heightMap.get(point[1]);
+                // found a building ending here
+                Integer cnt = heightMap.get(point.height);
                 if (cnt == 1) {
-                    heightMap.remove(point[1]);
+                    // there's only 1 building with this height, so when this building ends here, delete it from heightMap
+                    heightMap.remove(point.height);
                 } else {
-                    heightMap.put(point[1], cnt - 1);
+                    heightMap.put(point.height, cnt - 1);
                 }
             }
             
             int curr = heightMap.firstKey();
             if (prev != curr) {
-                res.add(new int[] {point[0], curr});
+                res.add(new int[] {point.x, curr});
                 prev = curr;
             }
         }
         
         return res;
+    }
+    
+    class Point {
+        int x, height;
+        public Point(int x, int height) {
+            this.x = x;
+            this.height = height;
+        }
     }
 }
